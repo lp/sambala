d111883da46e9020c849eb0c8ed943f4625776f5
lp <lp@spiralix.org>
2008-12-15 21:59:55 -0500

    method cleanup and 1st pass docs

--- lib/sambala_gardener.rb
+++ lib/sambala_gardener.rb
@@ -1,3 +1,8 @@
+# The Sambala::Gardener module bings Abundance into Sambala.
+# A bunch of mixins used inside Sambala to access Abundance Non-Blocking Threading.
+# These methods are a higher level abstraction of Abundance methods,
+# serving as reusable general purpose agents inside Sambala.
+# 
 # Author:: lp (mailto:lp@spiralix.org)
 # Copyright:: 2008 Louis-Philippe Perron - Released under the terms of the MIT license
 # 
@@ -7,23 +12,58 @@ class Sambala
   
   module Gardener
     
+    # The +execute+ method splits the execution according to the operation mode: queue or interactive.
+    # === Parameters
+    # * _command_ = the command as a string
+    # * _data_ = the command argument as a string
+    # * _queue_ = the command operation mode
+    # === Example
+    #   result = execute('cd','dir/otherDir*',false)  # =>  true
     def execute(command,data,queue)
-      puts "executing command: #{command} data: #{data} queue: #{queue}"
       (queue.is_a? TrueClass) ? exec_queue(command,data) : exec_interactive(command,data)
     end
+    
+    # The +execute_all+ method does a special command invocation where all smbclient workers running in parallel
+    # are all sent the same command.  The method returns one boolean value for all workers success.
+    # === Parameters
+    # * _command_ = the command as a string
+    # * _data_ = the command argument as a string
+    # === Example
+    #   result = execute_all('mask','match*')   # =>  true
+    def execute_all(command,data)
+      sleep 1
+      result = @gardener.seed_all("#{command} #{data}")
+      bools = result.map { |row| row[:success] }
+      bools.uniq.size == 1 ? true : false
+    end
 
+    # The +exec_interactive+ method follows +execute+ when queue=false.
+    # === Parameters
+    # * _command_ = the command as a string
+    # * _data_ = the command argument as a string
+    # === Example
+    #   result = exec_interactive('put','aFile.txt')    # =>  [false, "aFile.txt does not exist\r\n"]
     def exec_interactive(command,data)
       id = @gardener.seed("#{command} #{data}")
       result = @gardener.harvest(id)
       return result[:success], result[:message]
     end
 
+    # The +exec_queue+ method follows +execute+ when queue=true
+    # === Parameters
+    # * _command_ = the command as a string
+    # * _data_ = the command argument as a string
+    # === Example
+    #   result = exec_queue('get','aFile.txt')  # => [true,true]
     def exec_queue(command,data)
-      return @gardener.seed("#{command} #{data}")
+      @gardener.seed("#{command} #{data}").integer? ? [true,true] : [false,false]
     end
 
+    # The +init_gardener+ method initialize a gardener class object
     def init_gardener
-      @gardener = Abundance.gardener(:seed_size => 8192, :rows => @options[:threads], :init_timeout => 3) do
+      # To Implement:
+      # workgroup -W WORKGROUP
+      @gardener = Abundance.gardener(:seed_size => 8192, :rows => @options[:threads], :init_timeout => @options[:threads] + 1) do
         PTY.spawn("smbclient //#{@options[:host]}/#{@options[:share]} #{@options[:password]} -U #{@options[:user]}") do |r,w,pid|
           w.sync = true
           $expect_verbose = false
