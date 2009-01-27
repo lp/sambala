7b8fa73550a6afabe9680835109167fffd7131f8
lp <lp@spiralix.org>
2009-01-26 23:54:34 -0500

    cd and lcd as execute_all methods and docs for rmdir

--- lib/sambala.rb
+++ lib/sambala.rb
@@ -43,7 +43,7 @@ class Sambala
   # * :password = the password to log into the server
   # * :threads = how many parallel operations you want initiated, !!! higher than 4 at you own risk !!!
   # === Example
-  #   sam = Sambala.new(  :domain   =>  'NTDOMAIN', 
+  #   samba = Sambala.new(  :domain   =>  'NTDOMAIN', 
   #                       :host     =>  'sambaserver',
   #                       :share    =>  'sambashare',
   #                       :user     =>  'walrus', 
@@ -60,18 +60,15 @@ class Sambala
     end
   end
 
-  # The +cd+ instance method takes only one argument, the path to which you wish to change directory
-  # Its one of the only implemented command where queue mode is not available, for the simple reason that
-  # when queued operations are executed in parallel, one does not control which command will get executed first, 
-  # making a queued +cd+ operation very dangerous.  
+  # The +cd+ instance method takes only one argument, the path to which you wish to change directory  
   # === Parameters
   # * :to = the path to change directory to
   # === Interactive Returns
   # * _boolean_ = confirms if +cd+ operation completed successfully
   # === Example
-  #   sam.cd(:to => 'aFolder/anOtherFolder/')   # =>  true
+  #   samba.cd(:to => 'aFolder/anOtherFolder/')   # =>  true
   def cd(opts={:to => ''})
-    execute('cd', opts[:to], false)[0]
+		execute_all('cd', opts[:to])
   end
   
   # The +du+ instance does exactly what _du_ usually does: estimates file space usage.
@@ -80,7 +77,7 @@ class Sambala
   # === Interactive Returns
   # * _string_ = +du+ command results
   # === Example
-  #   puts sam.du   # =>  34923 blocks of size 2097152. 27407 blocks available
+  #   puts samba.du   # =>  34923 blocks of size 2097152. 27407 blocks available
   #                       Total number of bytes: 59439077
   def du(opts={:queue=>false})
     execute('du', '', opts[:queue])[1]
@@ -93,12 +90,12 @@ class Sambala
   # === Interactive Returns
   # * _boolean_ = confirms if +del+ operation completed successfully
   # === Example
-  #   sam.del(:mask => 'aFile')   # =>  true
+  #   samba.del(:mask => 'aFile')   # =>  true
   def del(opts={:mask => nil, :queue=>false})
     execute('del', opts[:mask], opts[:queue])[0]
   end
   alias rm del
-  # The maexist? instance method is borrowed from Ruby File Class idiome.
+  # The exist? instance method is borrowed from Ruby File Class idiome.
   # It is used to test the presence of files or directories on the server
   # === Parameters
   # * :mask = the mask matching the file or directory to look for.
@@ -106,7 +103,7 @@ class Sambala
   # === Interactive Returns
   # * _boolean_ = confirm the presence of a matching file or directory
   # === Example
-  #   sam.exist?(:mask => 'aFile')  # => true
+  #   samba.exist?(:mask => 'aFile')  # => true
   def exist?(opts={:mask => nil, :queue => false})
     execute('ls', opts[:mask], opts[:queue])[0]
   end
@@ -120,24 +117,21 @@ class Sambala
   # === Interactive Returns
   # _array_ = [ _booleanSuccess_, _getResultMessage_ ]
   # === Example
-  #   sam.get(:from => 'aFile.txt')   # => [true, "getting file \\aFile.txt.rb of size 3877 as test.rb (99.6 kb/s) (average 89.9 kb/s)\r\n"]
+  #   samba.get(:from => 'aFile.txt')   # => [true, "getting file \\aFile.txt.rb of size 3877 as test.rb (99.6 kb/s) (average 89.9 kb/s)\r\n"]
   def get(opts={:from => nil, :to => nil, :queue => false})
     opts[:to].nil? ? strng = opts[:from] : strng = opts[:from] + ' ' + opts[:to]
     execute('get', strng, opts[:queue])
   end
   
-  # The +lcd+ instance method changes the current working directory on the local machine to the directory specified.
-  # Its one of the only implemented command where queue mode is not available, for the simple reason that
-  # when queued operations are executed in parallel, one does not control which command will get executed first, 
-  # making a queued +lcd+ operation very dangerous.  
+  # The +lcd+ instance method changes the current working directory on the local machine to the directory specified. 
   # === Parameters
   # * :to = the path to change directory to
   # === Interactive Returns
   # * _boolean_ = confirms if +cd+ operation completed successfully
   # === Example
-  #   sam.lcd(:to => 'aLocalFolder/anOtherFolder/')   # => true
+  #   samba.lcd(:to => 'aLocalFolder/anOtherFolder/')   # => true
   def lcd(opts={:to => ''})
-    execute('lcd', opts[:to], false)[0]
+		execute_all('lcd', opts[:to])
   end
   
   # The +lowercase+ method toggles lowercasing of filenames for the get command.
@@ -146,7 +140,7 @@ class Sambala
   # === Interactive Returns
   # * _boolean_ = confirms if +lowercase+ operation completed successfully
   # === Example
-  #   sam.lowercase   # => true      # toggle from files all UPPERCASE to all lowercase
+  #   samba.lowercase   # => true      # toggle from files all UPPERCASE to all lowercase
   def lowercase
     execute_all('lowercase' ,'')
   end
@@ -158,7 +152,7 @@ class Sambala
   # === Interactive Returns
   # * _string_ = containing +ls+ command results
   # === Example
-  #   sam.ls  # =>  genpi.rb                            A       81  Mon Nov 17 22:12:40 2008
+  #   samba.ls  # =>  genpi.rb                            A       81  Mon Nov 17 22:12:40 2008
   #                     34923 blocks of size 2097152. 27407 blocks available
   def ls(opts={:mask => nil, :queue=>false})
     execute('ls' ,opts[:mask], opts[:queue])[1]
@@ -171,7 +165,7 @@ class Sambala
   # === Parameters
   # * :mask = the matching filter
   # === Example
-  #   sam.mask(:mask => 'filter*')  # => true
+  #   samba.mask(:mask => 'filter*')  # => true
   def mask(opts={:mask => nil})
     execute_all('mask' ,opts[:mask])
   end
@@ -184,7 +178,7 @@ class Sambala
   # === Interactive Returns
   # _array_ = [ _booleanSuccess_, _mgetResultMessage_ ]
   # === Example
-  #   sam.mget(:mask => 'file*')  # => [true, "getting file \\file_new.txt of size 3877 as file_new.txt (99.6 kb/s) (average 89.9 kb/s)\r\n"]
+  #   samba.mget(:mask => 'file*')  # => [true, "getting file \\file_new.txt of size 3877 as file_new.txt (99.6 kb/s) (average 89.9 kb/s)\r\n"]
   def mget(opts={:mask => nil, :queue => false})
     execute('mget' ,opts[:mask], opts[:queue])
   end
@@ -196,7 +190,7 @@ class Sambala
   # === Interactive Returns
   # * _boolean_ = confirms if +mkdir+ operation completed successfully
   # === Example
-  #   sam.mkdir(:path => 'aFolder/aNewFolder')  # => true
+  #   samba.mkdir(:path => 'aFolder/aNewFolder')  # => true
   def mkdir(opts={:path => '', :queue => false})
     execute('mkdir' ,opts[:path], opts[:queue])[0]
   end
@@ -210,7 +204,7 @@ class Sambala
   # === Interactive Returns
   # _array_ = [ _booleanSuccess_, _mputResultMessage_ ]
   # === Example
-  #   sam.mput(:mask => 'file*')  # =>  [true, "putting file \\file_new.txt of size 1004 as file_new.txt (65.4 kb/s) (average 65.4 kb/s)\r\n"]
+  #   samba.mput(:mask => 'file*')  # =>  [true, "putting file \\file_new.txt of size 1004 as file_new.txt (65.4 kb/s) (average 65.4 kb/s)\r\n"]
   def mput(opts={:mask => nil, :queue => false})
     execute('mput' ,opts[:mask], opts[:queue])
   end
@@ -224,7 +218,7 @@ class Sambala
   # === Interactive Returns
   # _array_ = [ _booleanSuccess_, _putResultMessage_ ]
   # === Example
-  #   sam.put(:from => 'aLocalFile.txt')   # =>  [false, "aLocalFile.txt does not exist\r\n"]
+  #   samba.put(:from => 'aLocalFile.txt')   # =>  [false, "aLocalFile.txt does not exist\r\n"]
 
   def put(opts={:from => nil, :to => nil, :queue => false})
     opts[:to].nil? ? strng = opts[:from] : strng = opts[:from] + ' ' + opts[:to]
@@ -236,11 +230,19 @@ class Sambala
   # === Interactive Returns
   # * _boolean_ = confirms if +mkdir+ operation completed successfully
   # === Example
-  #   sam.recurse   # => true
+  #   samba.recurse   # => true
   def recurse
     execute_all('recurse' ,'')
   end
 
+	# The +rmdir+ method deletes the specified directory
+	# === Parameters
+	# * :path = the relative path to the directory to be deleted
+  # * :queue = sets queue processing mode. Defaults to interactive mode when no option given.
+  # === Interactive Returns
+  # * _boolean_ = confirms if +rmdir+ operation completed successfully
+  # === Example
+	# 	samba.rmdir(:path => 'mydir')		# => true
 	def rmdir(opts={:path=>nil,:queue=>false})
 		execute('rmdir' ,opts[:path], opts[:queue])[0]
 	end
@@ -251,14 +253,14 @@ class Sambala
   # === Interactive Returns
   # * _string_ = containing +volume+ command results
   # === Example
-  #   sam.volume  # => "Volume: |geminishare| serial number 0x6d723053"
+  #   samba.volume  # => "Volume: |geminishare| serial number 0x6d723053"
   def volume(opts={:queue=>false})
     execute('volume' ,'', opts[:queue])[1]
   end
   
   # The +queue_results+ methods collect a done queue items results
   # === Example 
-  #   result = sam.queue_results
+  #   result = samba.queue_results
   def queue_results
     crop = @gardener.harvest(:full_crop)
     crop.map! { |result| [ result[:success], result[:seed], result[:message] ]  }
@@ -266,14 +268,14 @@ class Sambala
   
   # The +progress+ method returns a progress ratio indicator from 0.00 to 1.00
   # === Example
-  #   progress = sam.progress   # => 0.75
+  #   progress = samba.progress   # => 0.75
   def progress
     @gardener.growth(:progress)
   end
   
   # The +close+ method safely end the smbclient session
   # === Example
-  #   sam.close
+  #   samba.close
   def close
     result = @gardener.close
     result.values.map { |queue| queue.empty? }.uniq.size == 1 ? true : false
